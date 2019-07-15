de8006ab6dd23652b006fba9257189fb39f32538
wickdeeb <isak.m.h.s@gmail.com>
2019-07-15 17:12:55 +0200

    add post update

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -35,7 +35,21 @@ router.route('/:id').delete((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/update/:id').post((req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      post.author = req.body.author;
+      post.title = req.body.title;
+      post.conent = req.body.content;
+      post.comments = req.body.comments;
+      post.votes = Number(req.body.votes);
+      post.date = Date.parse(req.body.date);
 
-
+      post.save()
+        .then(() => res.json('Post updated!'))
+        .catch(err => res.status(400).json('Error: '+ err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
