2b084a06e0d73e74ec988f771622e1d80aa357f3
wickdeeb <isak.m.h.s@gmail.com>
2019-07-17 11:46:51 +0200

    add navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar(props) {
-  let loggedInText = (
-    <p>Logged in as: {props.state.username}</p>
-  );
-  return(
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/users">Users</Link>
-      <Link to="/register">Register</Link>
-      {props.state.loggedIn ? loggedInText : <p>Logged out</p>}
-    </nav>
-  );
+class Navbar extends React.Component {
+  dropdown = () => {
+    var navbar = document.getElementById('nav');
+    if (navbar.className === '') {
+      navbar.className = 'responsive';
+    } else {
+      navbar.className = '';
+    }
+  }
+  render() {
+    let loggedInText = (
+      <div className="account">My Account ({this.props.state.username})</div>
+    );
+
+    let signedInText = (
+      <div className="account">Sign in</div>
+    );
+    return(
+      <nav id="nav">
+        <Link to="/">Herddit</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/">{this.props.state.loggedIn ? loggedInText : signedInText}</Link>
+        <div className="icon" onClick={this.dropdown}>
+          <i className="fa fa-bars"></i>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
