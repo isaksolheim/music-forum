52e5a5fa600fce1fa234d6f6223991eb050bf8e9
wickdeeb <isak.m.h.s@gmail.com>
2019-07-17 16:07:02 +0200

    add landing mobile style

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 function Landing(props) {
   return(
-    <section>
+    <section className="landing">
       <h1>HERDDIT</h1>
-      <p>Welcome to Herddit</p>
-      <form onSubmit={props.submitHandler}>
+      <p>Welcome to Herddit, please sign in below or log in as a guest.</p>
+      
+      <form className="login-form" onSubmit={props.submitHandler}>
         <label> 
-          Username:
-          <input type="text" name="name" onChange={props.usernameHandler} />
+          Username
+          <input placeholder="username" type="text" name="name" onChange={props.usernameHandler} />
         </label>
         <label> 
-          Password:
-          <input type="text" name="name" onChange={props.passwordHandler} />
+          Password
+          <input className="password" placeholder="password" type="password" name="name" onChange={props.passwordHandler} />
         </label>
-        <input type="submit" value="Login" />
+        <input type="submit" value="Sign in" />
       </form>
     </section>
   );
