dcb71ada0e1d24efe173a0d4c2d202a63fd0b394
wickdeeb <isak.m.h.s@gmail.com>
2019-07-18 11:57:26 +0200

    fix navbar navigation and add profile picture to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
-  dropdown = () => {
+  dropdown = (i) => {
     var navbar = document.getElementById('nav');
-    if (navbar.className === '') {
-      navbar.className = 'responsive';
+
+    if (!i) {
+      if (navbar.className === '') {
+        navbar.className = 'responsive';
+      } else {
+        navbar.className = '';
+      }
     } else {
-      navbar.className = '';
+      navbar.className= '';
     }
   }
   render() {
-    let loggedInText = (
-      <div className="account">My Account ({this.props.state.username})</div>
-    );
-
-    let signedInText = (
-      <div className="account">Sign in</div>
-    );
+    let loggedInText; 
+    if (this.props.state.username) {
+      loggedInText = (
+        <div className="account">[{this.props.state.username}]</div>
+      );
+    } else {
+      loggedInText = (<div></div>);
+    }
     return(
       <nav id="nav">
-        <Link to="/">Herddit</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/">{this.props.state.loggedIn ? loggedInText : signedInText}</Link>
-        <div className="icon" onClick={this.dropdown}>
-          <i className="fa fa-bars"></i>
+        <Link to="/" onClick={() => this.dropdown(1)} className="title">Herddit</Link>
+        <Link to="/posts" onClick={() => this.dropdown(0)}>Posts</Link>
+        <Link to="/users" onClick={() => this.dropdown(0)}>Users</Link>
+        {this.props.state.loggedIn ? <div></div> : <Link to="/register" onClick={() => this.dropdown(0)}>Register</Link>}
+        <div className="right-icons">
+          <div className="profile">
+            {loggedInText}<i className="fa fa-user-circle fa-2x"></i>
+          </div>
+          <div className="icon" onClick={() => this.dropdown(0)}>
+            <i className="fa fa-bars fa-2x"></i>
+          </div>
         </div>
       </nav>
     );
